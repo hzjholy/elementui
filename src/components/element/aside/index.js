fdc57825fe4abb025c8dfc0fe2e8771908785cfc
hzjholy <41406542+hzjholy@users.noreply.github.com>
2024-02-18 21:31:38 +0800

    3.a

---
+++ src/components/element/aside/index.js
@@ -0,0 +1,8 @@
+import Aside from "./src/Aside.vue";
+
+
+Aside.install = (Vue) => {
+    Vue.component(Aside.name,Aside)
+}
+
+export default Aside;
---
+++ src/components/element/header/index.js
@@ -0,0 +1,8 @@
+import Header from "./src/Header.vue";
+
+
+Header.install = (Vue) => {
+    Vue.component(Header.name,Header)
+}
+
+export default Header;
