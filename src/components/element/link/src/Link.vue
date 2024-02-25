5f3beb790c3a7455f253a3ad71f4bafc1c923a43
hzjholy <41406542+hzjholy@users.noreply.github.com>
2024-02-25 15:58:33 +0800

    8.b

--- src/components/element/link/src/Link.vue
+++ src/components/element/link/src/Link.vue
@@ -1,9 +1,34 @@
 <template>
-  <a class="el-link"> <slot /> </a>
+  <a
+    class="el-link"
+    :class="[
+      type ? `el-link--${type}` : '',
+      {
+        'is-underline': underline && !disabled,
+        'is-disabled': disabled,
+      },
+    ]"
+    :herf="herf"
+  >
+    <i v-if="icon" :class="icon"></i>
+    <span v-if="$slots.default">
+      <slot />
+    </span>
+  </a>
 </template>
 
 <script>
 export default {
   name: "ElLink",
+  props: {
+    herf: String,
+    underline: {
+      type: Boolean,
+      default: true,
+    },
+    type: String,
+    disabled: Boolean,
+    icon: String,
+  },
 };
 </script>
